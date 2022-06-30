4ecdf210f313ef947b183dbc2443b85dd1a26861
fawl_03 <razidias@gmail.com>
2022-06-30 18:57:27 -0300

    att 1.5.1

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,18 +1,16 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  const { species } = data;
-  const array = {};
-  const objVazio = {};
-  if (animal.length === 0) {
-    return objVazio;
+const { species } = data;
+
+function countAnimals(animals) {
+  const obj = {};
+  if (!animals) {
+    species.forEach((item) => {
+      obj[`${item.name}`] = item.residents.length;
+    });
+    return obj;
   }
-  species.forEach((especie) => {
-    array[especie.name] = especie.residents.length;
-  });
-  console.log(array);
-  const filtroArray = array.filter((specie) => specie.name === animal);
-  console.log(filtroArray);
 }
+countAnimals();
 
 module.exports = countAnimals;
--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 function getAnimalsOlderThan(animal, age) {
   const itens = data.species.find((specie) => specie.name === animal);
   const residentesIdades = itens.residents.every((idade) => idade.age >= age);
-  return residentesIdades;
+  console.log(residentesIdades);
 }
-
+getAnimalsOlderThan('otters', 7);
 module.exports = getAnimalsOlderThan;

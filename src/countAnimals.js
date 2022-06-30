5f81ee0933431110b455b6368f916717c09ee051
fawl_03 <razidias@gmail.com>
2022-06-30 19:41:28 -0300

    att 1.5.2

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -10,7 +10,16 @@ function countAnimals(animals) {
     });
     return obj;
   }
+  if (!animals.sex) {
+    const ani = species.find((especie) => especie.name === animals.specie);
+    return ani.residents;
+  }
+  if (!animals.sex === false) {
+    const ani = species.find((especie) => especie.name === animals.specie);
+    const aniSexu = ani.residents.filter((secsu) => secsu.sex === animals.sex);
+    return aniSexu;
+  }
 }
-countAnimals();
+console.log(countAnimals());
 
 module.exports = countAnimals;

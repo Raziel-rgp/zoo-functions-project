3491bc61c5ec90832b0b4cc32f1eaa6914383742
fawl_03 <razidias@gmail.com>
2022-07-01 17:30:13 -0300

    att 1.9.2 100 testes

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-useless-escape */
 const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {
@@ -23,7 +22,19 @@ describe('Testes da função getOpeningHours', () => {
   it('Teste se caso passado Wednesday as 9 da noite irá retornar The zoo is closed ', () => {
     expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
   });
-  it('Testa se caso não seja passado um horario retorna um erro', () => {
-    expect(getOpeningHours('Wednesday', '09:00-ae')).toThrowError(new Error('The abbreviation must be \'AM\' or \'PM\''));
-  })
+  it('Testa se caso não seja passado horario correto, retorna um erro', () => {
+    expect(() => { getOpeningHours('Wednesday', '09:00-ae'); }).toThrowError(new Error('The abbreviation must be \'AM\' or \'PM\''));
+  });
+  it('Testa se caso não seja passado horario corretor, retorna um erro', () => {
+    expect(() => { getOpeningHours('Wednesday', '23:00-AM'); }).toThrowError(new Error('The hour must be between 0 and 12'));
+  });
+  it('Testa se caso não seja passado horario corretor, retorna um erro', () => {
+    expect(() => { getOpeningHours('Wednesday', '12'); }).toThrowError(new Error('The minutes should represent a number'));
+  });
+  it('Testa se caso não seja passado horaio corretor, retorna um erro', () => {
+    expect(() => { getOpeningHours('Wednesday', '12:123-AM'); }).toThrowError(new Error('The minutes must be between 0 and 59'));
+  });
+  it('Testa se caso não seja passado horaio corretor, retorna um erro', () => {
+    expect(() => { getOpeningHours('SEGUYNDA', '9:00-AM'); }).toThrowError(new Error('The day must be valid. Example: Monday'));
+  });
 });

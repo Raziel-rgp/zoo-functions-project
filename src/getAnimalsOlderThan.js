2105eedb96f2a64faf281d562fca5629c82a4208
fawl_03 <razidias@gmail.com>
2022-07-01 15:23:53 -0300

    att 1.5.7.2 corrigindo getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,6 @@ const data = require('../data/zoo_data');
 function getAnimalsOlderThan(animal, age) {
   const itens = data.species.find((specie) => specie.name === animal);
   const residentesIdades = itens.residents.every((idade) => idade.age >= age);
-  console.log(residentesIdades);
+  return residentesIdades;
 }
-getAnimalsOlderThan('otters', 7);
 module.exports = getAnimalsOlderThan;
